e02d9fd10f8785fe41cf592031beec7a7024c82e
Cem Telliagaoglu <cem.telliagaoglu@gmail.com>
2022-11-23 15:32:52 +0300

    Solved Day1

--- HackerRank-30DaysOfCode.playground/Pages/Day1.xcplaygroundpage/Contents.swift
+++ HackerRank-30DaysOfCode.playground/Pages/Day1.xcplaygroundpage/Contents.swift
@@ -1,7 +1,22 @@
-//: [Previous](@previous)
-
 import Foundation
 
-var greeting = "Hello, playground"
+var i = 4
+var d = 4.0
+var s = "HackerRank "
+// Declare second integer, double, and String variables.
+var inpInt:Int = 0
+var inpDbl:Double = 0
+var inpStr:String = ""
 
-//: [Next](@next)
+// Read and save an integer, double, and String to your variables.
+inpInt = Int(readLine()!)!
+inpDbl = Double(readLine()!)!
+inpStr = readLine()!
+
+// Print the sum of both integer variables on a new line.
+print(i + inpInt)
+// Print the sum of the double variables on a new line.
+print(d + inpDbl)
+// Concatenate and print the String variables on a new line
+print(s + inpStr)
+// The 's' variable above should be printed first.
